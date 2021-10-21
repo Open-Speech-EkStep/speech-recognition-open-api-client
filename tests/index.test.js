475a332b3ecc11e71f8f2bf612ecfe3c1b5e1964
ankit-thoughtworks <ankit.katiyar@thoughtworks.com>
2021-10-21 14:49:11 +0530

    chore:setup jest

---
+++ tests/index.test.js
@@ -0,0 +1,9 @@
+describe('test index.js', function () {
+
+    describe('setStateOnMicStart', function () {
+        it('should set mic states', function () {
+            console.log('hey~~~######');
+        });
+    });
+
+});
